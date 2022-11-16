b8bdbe4837841282e4cf655ddb89fee68d7177ec
Rohan <rohanupadhyay@Rohans-Air.dyn-3224.ulm.edu>
2022-11-16 11:59:27 -0600

    Added grid

--- Assets/Scripts/PlayManager.cs
+++ Assets/Scripts/PlayManager.cs
@@ -49,7 +49,7 @@ public class PlayManager : MonoBehaviour
 
     void spawnCircle()
     {
-        GameObject.Instantiate(circle, transform.position, Quaternion.identity);
+        GameObject.Instantiate(circle, , Quaternion.identity);
     }
 
     void spawnCross()
