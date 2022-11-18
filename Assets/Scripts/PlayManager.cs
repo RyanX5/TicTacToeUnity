eb334e7036ee05ed91e3f885550cad93092df209
Rohan <rohanupadhyay@Rohans-Air.dyn-641.ulm.edu>
2022-11-17 19:50:10 -0600

    Finalized logic

--- Assets/Scripts/PlayManager.cs
+++ Assets/Scripts/PlayManager.cs
@@ -2,15 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
 
 public class PlayManager : MonoBehaviour
 {
     int[,] mat = new int[3, 3];
     public int moveCount = 0;
     [SerializeField] GameObject gameOverText;
+    [SerializeField] GameObject gameWonText;
     void Start()
     {
         gameOverText.SetActive(false);
+        gameWonText.gameObject.SetActive(false);
     }
 
     public void BackToMainMenu()
@@ -54,12 +58,85 @@ public class PlayManager : MonoBehaviour
 
         mat[currR, currC] = currVal;
 
+        if (WinCondition(mat))
+        {
+            gameWonText.GetComponent<TextMeshProUGUI>().SetText("GAME WON!");
+            gameWonText.gameObject.SetActive(true);
+        }
+        else
+        {
+            
+        }
+
+    }
+
+    bool WinCondition(int[,] arr)
+    {
+        bool isWon = false;
+
+        //Checks for rows
         for (int i = 0; i < 3; ++i)
         {
-            for(int j = 0; j < 3; ++j)
+            if (arr[i, 0] != 0)
             {
-                print(i +","+ j+" = " + mat[i, j]);
+                if (arr[i, 0] == arr[i, 1])
+                {
+                    if (arr[i, 1] == arr[i, 2])
+                    {
+                        isWon = true;
+                        print("Won by rows");
+                    }
+
+                }
             }
         }
+
+        //Checks for columns
+        for(int i = 0; i < 3; ++i)
+        {
+            if(arr[0, i] != 0)
+            {
+                if (arr[0, i] == arr[1, i])
+                {
+                    if (arr[1, i] == arr[2, i])
+                    {
+                        isWon = true;
+                        print("Won by columns");
+                    }
+
+                }
+            }
+        }
+
+        //Checks for diagonals (top-to-bot)
+        if(arr[0, 0] != 0)
+        {
+            if (arr[0, 0] == arr[1, 1])
+            {
+                if (arr[1, 1] == arr[2, 2])
+                {
+                    isWon = true;
+                    print("Won by diagonals");
+                }
+            }
+
+        }
+
+        //Checks for diagonals (topright-to-bot)
+        if (arr[0, 2] != 0)
+        {
+            if (arr[0, 2] == arr[1, 1])
+            {
+                if (arr[1, 1] == arr[2, 0])
+                {
+                    isWon = true;
+                    print("Won by diagonals");
+                }
+            }
+
+        }
+
+
+        return isWon;
     }
 }
